0a2febcabe84c1d5c4f033c901a0411540754480
jh <>
2024-08-21 14:12:37 -0700

    child nodes can toggle themselves

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 export type Tree = {
   name: string;
   checked?: boolean;
   children?: Tree[];
 };
 
-export const Tree = ({ name, children }: Tree) => {
+export const Tree = ({ name, children: initialChildren }: Tree) => {
+  const [children, setChildren] = useState(
+    initialChildren?.map((child) => ({
+      ...child,
+      checked: child.checked ?? false,
+    })),
+  );
   const handleParentToggle = () => {};
-  const handleChildToggle = (id: number) => {};
+  const handleChildToggle = (id: number) => {
+    setChildren(
+      children?.map((child, index) =>
+        index === id ? { ...child, checked: !child.checked } : child,
+      ),
+    );
+  };
+
   return (
     <div>
       <div className="flex items-center gap-2">
